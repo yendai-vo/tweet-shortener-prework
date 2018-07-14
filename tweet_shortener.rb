16a4b05efecbbc36e49844b6192c3257f4cd6b89
Cindy Vo <yendai.vo@hotmail.com>
2018-07-14 19:30:18 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+
+def dictionary(word)
+  word_list = {
+    "hello": 'hi',
+    "to": '2',
+    "two": '2',
+    "too": '2' ,
+    "for": '4',
+    "four": '4',
+    'be': 'b',
+    'you': 'u',
+    "at": "@" ,
+    "and": "&"
+  }
+
+  word_list[word]
+end
