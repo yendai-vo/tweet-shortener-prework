a1840f60d7b13213c6889dc79ba6fb7005450a2e
Cindy Vo <yendai.vo@hotmail.com>
2018-07-14 21:24:38 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,17 +1,21 @@
 
-def dictionary(word)
-  word_list = {
-    "hello": 'hi',
-    "to": '2',
-    "two": '2',
-    "too": '2' ,
-    "for": '4',
-    "four": '4',
-    'be': 'b',
-    'you': 'u',
-    "at": "@" ,
-    "and": "&"
+def dictionary
+  dictionary = {
+    "hello" => 'hi',
+    "to" =>'2',
+    "two" => '2',
+    "too" => '2',
+    "for" => '4',
+    "four" => '4',
+    "be" => 'b',
+    "you" => 'u',
+    "at" => "@",
+    "and" => "&"
   }
 
   word_list[word]
+end
+
+def word_substituter(string)
+  
 end
