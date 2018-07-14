0c3cd91d3d6c05d518cce0fb11652b51107de9a3
Cindy Vo <yendai.vo@hotmail.com>
2018-07-14 21:41:33 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -38,3 +38,11 @@ def selective_tweet_shortener(tweet)
     tweet
   end
 end
+
+def shortened_tweet_truncator(tweet)
+  if word_substituter(tweet).length > 140
+    word_substituter(tweet)[0..137]+ "..."
+  else
+    tweet
+  end
+end
